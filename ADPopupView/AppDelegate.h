615d374a498ab3e42dbf695ed4ca00a8025bc715
Anton Domashnev <antondomashnev@gmail.com>
2013-02-26 16:57:22 +0400

    Initial Commit

---
+++ ADPopupView/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  ADPopupView
+//
+//  Created by Anton Domashnev on 26.02.13.
+//  Copyright (c) 2013 Anton Domashnev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ ADPopupView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ADPopupView
+//
+//  Created by Anton Domashnev on 26.02.13.
+//  Copyright (c) 2013 Anton Domashnev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
